b38d7ea3110216421c5086541ce56d7f1b8a3f9e
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-05-12 11:59:40 +0300

    python-p3-anagram-detector-lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+        self.word_sorted = sorted(word)
+
+    def match(self, word_list):
+        matching_words = []        
+        for word in word_list:
+            if self.word != word and sorted(word) == self.word_sorted:
+                matching_words.append(word)
+        return matching_words
+        
